8c1b36b45145e012ef22fe39612287dfb69def5d
PieterVanh <pieter.vanh@gmail.com>
2014-04-21 00:05:36 +0200

    first pass add_match

--- includes/xmlrpc_function.php
+++ includes/xmlrpc_function.php
@@ -5,7 +5,7 @@ add_filter( 'xmlrpc_methods', 'add_xml_rpc_methods' );
 function add_xml_rpc_methods( $methods ) {
 
     $methods['frs.helloWorld'] = 'hello_world';
-    $methods['pltm.addMatch'] = 'add_match';
+    $methods['pltm.addMatch'] = 'pltm_add_match';
 
     return $methods;
 }
@@ -40,67 +40,41 @@ function hello_world( $params ) {
 
 
 
-function add_match($params){
 
+function pltm_add_match( $data ){
     global $wp_xmlrpc_server;
-
-    $blog_id  = (int) $params[0]; // not used, but follow in the form of the wordpress built in XML-RPC actions
-    $username = $params[1];
-    $password = $params[2];
-    $args     = $params[3];
-
-    // verify credentials
-    if ( ! $wp_xmlrpc_server->login( $username, $password ) ) {
-        return $wp_xmlrpc_server->error;
-    }
-
-    // check for edit_posts capability (requires contributor role)
-    // (obviously not required for this simple example, but just for demonstration purposes)
-    if ( ! current_user_can( 'edit_posts' ) )
-        return new IXR_Error( 403, __( 'You are not allowed access to details about orders.' ) );
-
-    do_action( 'xmlrpc_call', 'pltm.addMatch' ); // patterned on the core XML-RPC actions
-
-    // required parameter
-    //if ( ! isset( $args['name'] ) ) return new IXR_Error( 500, __( "Missing parameter 'name'" ) );
-
-
-    $challonge_tournament_id = $args['challonge_tournament_id'];
-    $wp_tournament_id        = tournamentCPT::get_tournament_id_by($challonge_tournament_id);
-
-    //if return false then no tournament with that challonge tournament id was found return error!
-    if(!$wp_tournament_id)
-        return new IXR_Error( 500, __( "No tournament with that challonge tournament id (".$challonge_tournament_id.") was found" ));
+    $args = json_decode($data,true);
 
     //todo added more complex naming, for more than 1v1 and teams.. not sure on limit
-    $match_name = sprintf('s% vs s%', $args['player_1'], $args['player_2']);
+    $match_name = sprintf('%s vs %s', $args["player_1"], $args["player_2"]);
 
     $new_match = array(
         'post_type'  => matchCPT::$post_type,
         'post_title' => $match_name,
-        'post_name'  => $args['pa_stats_match_id']
+        'post_name'  => $args["challonge_match_id"]
     );
 
     $match_id = wp_insert_post($new_match);
 
     //any other meta we need to attach to matches?????
-    update_post_meta($match_id, 'challonge_match_id', $args['challonge_match_id']);
-    update_post_meta($match_id, 'challonge_tournament_id', $args['challonge_tournament_id']);
-    update_post_meta($match_id, 'pa_stats_match_id', $args['pa_stats_match_id']);
+    update_post_meta($match_id, 'challonge_match_id', $args["challonge_match_id"]);
+    update_post_meta($match_id, 'challonge_tournament_id', $args["challonge_tournament_id"]);
+    update_post_meta($match_id, 'pa_stats_match_id', $args["pa_stats_match_id"]);
 
     //todo might be wise to add round? or should we write a function to work it out
     $connection_meta = array(
         'date'                     => current_time('mysql'),
-        'challonge_tournament_id'  => $challonge_tournament_id
+        'challonge_tournament_id'  => $args["challonge_tournament_id"]
     );
 
     //todo should be be able to link matches to matches so we can create chains? in future.
-    $p2p_result = p2p_type('tournament_matches')->connect($wp_tournament_id, $match_id, $connection_meta);
+    $p2p_result = p2p_type('tournament_matches')->connect($args["wp_post_id"], $match_id, $connection_meta);
 
     //todo should planets from the planetCPT be attach so on the match page we can show the planet....
 
     //attach players to this match
-    foreach($args['players'] as $key => $player ){
+
+    foreach($args["players"] as $key => $player ){
 
         //team is simple int, for example if its a ffa each player team would just be a int in a series, if team play, 2 players would be team int 1 and 2 team int 2
         $connection_meta = array(
@@ -120,6 +94,8 @@ function add_match($params){
         $p2p_result = p2p_type('match_players')->connect($match_id, $wp_player_id, $connection_meta);
 
     }
+
+    return "match added";
 
 }
 
