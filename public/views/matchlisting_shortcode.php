48380c128aa69344a57105342dbe2d09aabbb695
PieterVanh <pieter.vanh@gmail.com>
2014-05-13 20:25:54 +0200

    Show Round 1 Names Unspoilered

--- public/views/matchlisting_shortcode.php
+++ public/views/matchlisting_shortcode.php
@@ -20,10 +20,12 @@
 <script type="text/html" id="matchTemplate">
 <tr>
     <td><a data-bind="attr: {href: match_url}"><span data-bind="text: title"></span></a></td>
-    <td><span class="spoiler" data-bind="text: player1, css:{ matchwinner: player1() === winner()}"></span></td>
-    <td><span class="spoiler" data-bind="text: player2, css:{ matchwinner: player2() === winner()}"></span></td>
-    <td><a data-bind="if:pasduration, attr: {href: paslink}" target="_blank"><span data-bind="text:pasduration"></span>&nbsp; <?php echo '<img src="' . plugins_url( '../../public/assets/img/pastatssmall.png' , __FILE__ ) . '" border="0" title="pastats" alt="pastats">'; ?></a></td>
-    <td><a data-bind="if:twitch,attr: {href: twitch}" target="_blank"><img src="twitchblack.png" border="0" title="twitch" alt="twitch"></a></td>
+    <td><span data-bind="text: player1, css:{ spoiler: match_round() !== '1'}"></span><span class="spoiler" data-bind="css:{ matchwinner: player1() === winner()}"/><span></td>
+    <td><span data-bind="text: player2, css:{ spoiler: match_round() !== '1'}"></span><span class="spoiler" data-bind="css:{ matchwinner: player2() === winner()}"/><span></td>
+    <td><span data-bind="text:pasduration"></span></td>
+    <td>
+    <a data-bind="if:pasduration, attr: {href: paslink}" target="_blank"><?php echo '<img src="' . plugins_url( '../../public/assets/img/pastatssmall.png' , __FILE__ ) . '" border="0" title="pastats" alt="pastats">'; ?></a>
+    <a data-bind="if:twitch,attr: {href: twitch}" target="_blank"><?php echo '<img src="' . plugins_url( '../../public/assets/img/twitchblack.png' , __FILE__ ) . '" border="0" title="twitch" alt="twitch">'; ?></a></td>
     <!--<td data-bind="text: last_update"></td>-->
     <!--<td data-bind="text: now"></td>-->
 </tr>
