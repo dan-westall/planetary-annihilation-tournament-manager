361b51355f742d6655ee7a8de2246c2088f6c5ac
PieterVanh <pieter.vanh@gmail.com>
2014-05-13 19:59:48 +0200

    PASTATS Icon on matches

--- public/views/matchlisting_shortcode.php
+++ public/views/matchlisting_shortcode.php
@@ -22,7 +22,7 @@
     <td><a data-bind="attr: {href: match_url}"><span data-bind="text: title"></span></a></td>
     <td><span class="spoiler" data-bind="text: player1, css:{ matchwinner: player1() === winner()}"></span></td>
     <td><span class="spoiler" data-bind="text: player2, css:{ matchwinner: player2() === winner()}"></span></td>
-    <td><a data-bind="if:pasduration, attr: {href: paslink}" target="_blank"><span data-bind="text:pasduration"></span>&nbsp; <img src="pastatssmall.png" border="0" title="pastats" alt="pastats"/></a></td>
+    <td><a data-bind="if:pasduration, attr: {href: paslink}" target="_blank"><span data-bind="text:pasduration"></span>&nbsp; <?php echo '<img src="' . plugins_url( '../../public/assets/img/pastatssmall.png' , __FILE__ ) . '" border="0" title="pastats" alt="pastats">'; ?></a></td>
     <td><a data-bind="if:twitch,attr: {href: twitch}" target="_blank"><img src="twitchblack.png" border="0" title="twitch" alt="twitch"></a></td>
     <!--<td data-bind="text: last_update"></td>-->
     <!--<td data-bind="text: now"></td>-->
