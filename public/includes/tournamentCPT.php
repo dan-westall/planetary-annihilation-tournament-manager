d7218d513eb40d9f7289b1f734358fc02093bad8
Dan Westall <dan.westall@googlemail.com>
2014-07-10 18:47:18 +0100

    removed tournament endpoint on titles filter

--- public/includes/tournamentCPT.php
+++ public/includes/tournamentCPT.php
@@ -35,7 +35,7 @@ class tournamentCPT {
         add_action( 'save_post',  array( $this, 'delete_tournament_caches') );
 
         add_filter( 'tournament_rounds', array( $this, 'filter_tournament_rounds' ) );
-        add_filter( 'the_title', array( $this, 'filter_endpoint_titles'), 10, 2 );
+        //add_filter( 'the_title', array( $this, 'filter_endpoint_titles'), 10, 2 );
         //add_filter( 'single_template', array( $this, 'single_tournament_template') );
         add_filter( 'post_updated_messages', array( $this, 'filter_post_type_feedback_messages') );
 
