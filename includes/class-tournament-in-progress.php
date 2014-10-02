2836d31f2e2bc441c8685d99d79c090ed9f4a6c5
Dan Westall <dan.westall@googlemail.com>
2014-10-02 01:12:06 +0100

    tournament in progress API added

--- includes/class-tournament-in-progress.php
+++ includes/class-tournament-in-progress.php
@@ -13,7 +13,29 @@ class tournament_in_progress {
     }
 
 
-    public static function realtime_update_live_page(){
+    public static function realtime_update_live_page($post_id){
+
+        if (!class_exists('ZMQContext'))
+            return false;
+
+        if(get_post_meta($post_id, '_wp_page_template', true) != 'template-tournament-in-progress-2.php')
+            return false;
+
+        $tournament_id = get_post_meta($post_id, 'tournament', true );
+
+        //setsub
+        $result['subscription'] =  sprintf('t%s-live', $tournament_id);
+
+
+
+        //send to realtime
+        $context = new ZMQContext();
+        $socket  = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
+        $socket->connect("tcp://localhost:5555");
+
+        $socket->send(json_encode($result));
+
+
 
     }
 
