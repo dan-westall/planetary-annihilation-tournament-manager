3ffe60741480b58bd2455b87d42b5c01a3359a1c
Dan Westall <dan.westall@googlemail.com>
2014-10-12 15:27:49 +0100

    switch team

--- public/includes/matchCPT.php
+++ public/includes/matchCPT.php
@@ -652,7 +652,7 @@ class matchCPT {
             $_post['meta']['match_round']    = get_post_meta($post['ID'], 'match_round', true);
             $_post['meta']['team_filter']    = get_post_meta($post['ID'], 'team_filter', true);
             $_post['meta']['format']         = self::match_format($post['ID']);
-            $_post['meta']['teams']          = self::get_clan_team_from_match($post['ID']);
+            $_post['meta']['teams']          = self::get_clan_team_from_match($post['ID'], true);
 
 
 
@@ -990,7 +990,7 @@ class matchCPT {
 
     }
 
-    public static function get_clan_team_from_match($match_id){
+    public static function get_clan_team_from_match($match_id, $swtich = false){
 
         global $wpdb;
 
@@ -999,7 +999,13 @@ class matchCPT {
         $match_teams = self::get_match_players($match_id);
 
         foreach($match_teams as $team){
-            $clan_teams[$team->clan] = $team->team;
+
+            if($swtich){
+                $clan_teams[$team->team] = $team->clan;
+            } else {
+                $clan_teams[$team->clan] = $team->team;
+
+            }
         }
 
         return $clan_teams;
