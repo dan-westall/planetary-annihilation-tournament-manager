fc4e72bf6b4b01ca924a6f765c42e45a14999488
Dan Westall <dan.westall@googlemail.com>
2015-02-13 02:02:02 +0000

    new methods and logic for player signups (sip)

--- includes/class-signup.php
+++ includes/class-signup.php
@@ -113,49 +113,166 @@ class tournamentSignup {
 
     }
 
-    public function is_existing_player($value){
+    public function is_existing_player($values){
 
         global $wpdb;
 
-        $player = $wpdb->get_row( $wpdb->prepare("SELECT user_email, ID AS user_id, (SELECT meta_value FROM wp_usermeta  WHERE user_id = user.ID AND meta_key = 'player_id') AS player_id  FROM $wpdb->users AS user WHERE user_email = %s", $values['email']['value']) );
+        $player = $wpdb->get_row(
+            $wpdb->prepare(
+                "
+                SELECT
+                  user_email,
+                  ID AS user_id,
+                  (SELECT meta_value FROM wp_usermeta  WHERE user_id = user.ID AND meta_key = 'player_id') AS player_id
+                    FROM $wpdb->users AS user
+                      WHERE user_email = %s
+                ",
+                $values['email']
+            )
+        );
 
         //do name check
         if(!isset($player->player_id)){
 
-            $player = $wpdb->get_row( $wpdb->prepare("SELECT user_email, post.ID, (SELECT meta_value FROM wp_postmeta  WHERE post_id = post.ID AND meta_key = 'user_id') AS user_id  FROM wp_posts AS post LEFT JOIN wp_users AS user ON user.ID = (SELECT meta_value FROM wp_postmeta  WHERE post_id = post.ID AND meta_key = 'user_id') WHERE post_title = '%s' AND user_email != ''", $values['ign']['value']) );
+            $player = $wpdb->get_row(
+                $wpdb->prepare(
+                    "
+                    SELECT
+                        user_email,
+                        post.ID,
+                        (SELECT meta_value FROM wp_postmeta  WHERE post_id = post.ID AND meta_key = 'user_id') AS user_id
+                          FROM wp_posts AS post
+                            LEFT JOIN wp_users AS user ON user.ID = (SELECT meta_value FROM wp_postmeta  WHERE post_id = post.ID AND meta_key = 'user_id')
+                              WHERE post_title = '%s'
+                                AND user_email != ''
+                    ",
+                    $values['ign']
+                )
+            );
+
 
         }
+
+        return false;
+
+    }
+
+    public function is_excluded_player($values){
+
+        global $wpdb;
+
+        $excluded_players_list = $wpdb->query(
+            $wpdb->prepare(
+                "
+                SELECT
+                user_email
+                    FROM $wpdb->users AS user WHERE user.ID IN ( SELECT ( SELECT meta_value FROM $wpdb->postmeta WHERE post_id = p2p_to AND meta_key = 'user_id') FROM wp_p2p  WHERE p2p_type = 'tournament_excluded_players' AND p2p_from = %s)
+                ",
+                $this->tournament_id
+            )
+        );
+
+        //if email is in excluded players bin, if there are any
+        if(is_array($excluded_players_list)){
+            if (in_array($values['email'], $excluded_players_list))
+                return true;
+
+        }
+
+        return false;
+    }
+
+    public function is_tournament_signup_open($tournament_id){
+
+        $tournament_closed        = get_post_meta($tournament_id, 'signup_closed', true);
+        $tournament_slots         = get_post_meta($tournament_id, 'slots', true);
+        $tournament_reserve_slots = get_post_meta($tournament_id, 'reserve_slots', true);
+        $tournament_status        = get_post_meta($tournament_id, 'tournament_status', true);
+        $total_tournament_slots   = ($tournament_slots + $tournament_reserve_slots);
+        $tournament_player_status = tournamentCPT::$tournament_player_status;
+
+        if($tournament_closed == true || $tournament_status >= 1){
+
+            return false;
+
+        }
+
+        if(tournamentCPT::get_tournament_player_count($tournament_id, [$tournament_player_status[0], $tournament_player_status[1]]) >= $total_tournament_slots){
+
+            return false;
+
+        }
+
+        return true;
+
+    }
+
+    public function join_tournament(){
+
+
+    }
+
+    public function join_team(){
 
     }
 
     public static function player_signup(){
 
-        secutiry();
-
+        check_ajax_referer('security-' . date('dmy'), 'security');
 
         $tournament_id = $_POST['tournament_id'];
         $player_id     = $_POST['player_id'];
-        $signup        = $_POST['signup_data'];
+        $signup_data   = $_POST['signup_data'];
 
 
         $signup = new tournamentSignup();
-
         $signup->setTournamentId($tournament_id);
 
+        try{
 
-        if($signup->is){
+            if(!self::is_tournament_signup_open($tournament_id))
+                throw new Exception('Tournament sign ups closed.');
 
-        }
 
-        if(false === ( $player_id = $signup->is_existing_player($signup) )){
+            if(false === ( $player_id = $signup->is_existing_player($signup_data) )){
 
-            //ok this is not an existing player we need to make an account!, call to playerCPT
+                //ok this is not an existing player we need to make an account!, call to playerCPT
 
-            if(false === (  $user = get_user_by( 'email', $signup['email'] ) )){
-               $user =  $signup->new_user($signup);
+                if(false === (  $user = get_user_by( 'email', $signup['email'] ) )){
+                    $user =  $signup->new_user($signup_data);
+
+                    if (!is_object($user))
+                        throw new Exception('New user was not created.');
+
+                }
+
+                $player_id = playerCPT::new_player_profile($user->ID, $signup_data);
+
+                if (!is_int($player_id)){
+                    require_once(ABSPATH.'wp-admin/includes/user.php' );
+
+                    //if new user was created but a new player profile could not be created then we need to delete the now orphaned user, for tidy
+                    wp_delete_user( $user->ID );
+
+                    throw new Exception('Player Profile was not created.');
+                }
+
             }
 
-            $player_id = playerCPT::new_player_profile($user->ID,[]);
+            //if we are there, no exceptions so we have a new user with player profile, with a tournament that they can signup to
+            if($signup->is_excluded_player($player_id))
+                throw new Exception('Sorry but you are excluded from this tournament.');
+
+
+            $signup->join_tournament()->join_team();
+
+
+
+        } catch (Exception $e) {
+
+            wp_send_json_error(['message' => $e->getMessage()]);
+
+            die();
 
         }
 
@@ -164,13 +281,13 @@ class tournamentSignup {
     }
 
 
-    public function new_user(){
+    public function new_user($values){
 
         $password = wp_generate_password();
 
         $userdata = array(
-            'user_login' => $values['ign']['value'],
-            'user_email' => $values['email']['value'],
+            'user_login' => $values['ign'],
+            'user_email' => $values['email'],
             'user_pass'  => $password
         );
 
