7df23c403c9ef109958c30af0221a25e4f158008
Dan Westall <dan.westall@googlemail.com>
2015-02-20 01:54:12 +0000

    improved signup process

--- tmp/patm.signup.js
+++ tmp/patm.signup.js
@@ -1,4 +1,4 @@
-var signupForm = angular.module('main', []);
+var signupForm = angular.module('main', ['ngMessages']);
 
 
 signupForm.constant('wordpressJS', js);
@@ -9,32 +9,34 @@ signupForm.controller('signupFormController', [ '$scope', '$http', 'wordpressJS'
 
         $scope.signupData = {};
         $scope.message = '';
-        $scope.submit = function () {
-            $http({
-                method: 'POST',
-                url: wordpressJS.ajaxurl,
-                data: $.param({
-                    action: 'player_signup',
-                    security: wordpressJS.security,
-                    tournament_id: wordpressJS.post_id,
-                    user_id: wordpressJS.user_id
-                }),  // pass in data as strings
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}  // set the headers so angular passing info as form data (not request payload)
-            })
-                .success(function (data) {
+        $scope.submitSignup = function (formData, validity) {
+            if (validity) {
+                $http({
+                    method: 'POST',
+                    url: wordpressJS.ajaxurl,
+                    data: $.param({
+                        action: 'player_signup',
+                        security: wordpressJS.security,
+                        tournament_id: wordpressJS.post_id,
+                        user_id: wordpressJS.user_id,
+                        signup_data: formData
+                    }),  // pass in data as strings
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}  // set the headers so angular passing info as form data (not request payload)
+                })
+                    .success(function (data) {
 
-                    if (!data.success) {
-                        // if not successful, bind errors to error variables
-                        $scope.errorName = data.errors.name;
-                        $scope.errorSuperhero = data.errors.superheroAlias;
-                    } else {
-                        $scope.message = data.data.message;
-                        $scope.ordered = true;
-                    }
-                });
-        };
-
-
+                        if (!data.success) {
+                            // if not successful, bind errors to error variables
+                            $scope.message = data.data.message;
+                            console.log(data.data.message);
+                            //$scope.errorSuperhero = data.errors.superheroAlias;
+                        } else {
+                            $scope.message = data.data.message;
+                        }
+                    });
+            }
+            ;
+        }
     }
 ]);
 signupForm.directive('input', function ($parse) {
